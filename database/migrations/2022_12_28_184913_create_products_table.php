920b37d16151e11b31a1362dc176f4b72148fa1a
Akrez <akrez.like@gmail.com>
2023-02-06 21:42:37 +0330

    CreateProductsTable

---
+++ database/migrations/2022_12_28_184913_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->boolean('is_active')->nullable();
+            $table->string('title', 60);
+            $table->string('code', 60)->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->string('blog_name', 60)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
