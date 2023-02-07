8bc69e7c919ef02603c3af60f4059f6f9c8436d2
Akrez <akrez.like@gmail.com>
2023-02-08 02:46:15 +0330

    site

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -7,16 +7,25 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
-    <link rel="shortcut icon" href="{{asset('favicon.ico')}}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <title>@yield('title', config('app.name'))</title>
     <!-- Bootstrap 5 -->
-    <link rel="stylesheet" href="{{asset('dist/bootstrap-5.0.2-dist/css/bootstrap.rtl.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('dist/bootstrap-5.0.2-dist/css/bootstrap.rtl.min.css') }}">
     <!-- Font Sahel -->
-    <link rel="stylesheet" href="{{asset('dist/vazirmatn-v32.102/Vazirmatn-font-face.css')}}">
+    <link rel="stylesheet" href="{{ asset('dist/vazirmatn-v32.102/Vazirmatn-font-face.css') }}">
     <!-- All -->
-    <link rel="stylesheet" href="{{asset('dist/fontawesome-free-5.15.4-web/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('dist/fontawesome-free-5.15.4-web/css/all.min.css') }}">
+    <!-- All -->
+    <link rel="stylesheet" href="{{ asset('dist/bootstrap-social-rtl/bootstrap-social.css') }}">
     <!-- style -->
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <style>
+        .z-1030 {
+            z-index: 1030;
+        }
+    </style>
+
     @yield('POS_HEAD')
 </head>
 
@@ -24,42 +33,48 @@
 
     @yield('POS_BEGIN')
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3 rounded rounded-3">
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg mb-3 z-1030">
         <div class="container">
             <a class="navbar-brand" href="{{ route('home') }}">وبـلاگ فروشـگاهـی اکــرز</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 
                     @guest
-                    @if (Route::has('login'))
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @endif
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
 
-                    @if (Route::has('register'))
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                    </li>
-                    @endif
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
                     @else
-                    <li class="nav-item dropdown">
-
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            {{ Auth::user()->name }}
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('blogs.index') }}">{{ __('Blogs') }}</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                {{ Auth::user()->name }}
                             </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                     @endguest
                 </ul>
             </div>
@@ -67,22 +82,44 @@
     </nav>
 
     <div class="container">
+        @if (UserActiveBlog::has())
+            <nav class="navbar navbar-expand-sm navbar-light sticky-top text-dark bg-light rounded shadow-sm mb-3 px-2">
+                <a class="navbar-brand p-0 mx-3" href="#">{{ UserActiveBlog::attr('title') }}</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="mynavbar">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a href="{{ route('products.index') }}"
+                                class="nav-link">
+                                @lang('Products')
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        @endif
         @hasSection('header')
-        <h1>@yield('header')</h1>
+            <h1 class="fs-2 my-4">@yield('header')
+                @hasSection('subheader')
+                    <small class="text-muted">@yield('subheader')</small>
+                @endif
+            </h1>
         @endif
         @yield('content')
     </div>
 
     <!-- jQuery -->
-    <script src="{{asset('dist/jquery-3.6.0/jquery.min.js')}}"></script>
+    <script src="{{ asset('dist/jquery-3.6.0/jquery.min.js') }}"></script>
     <!-- jQuery UI 1.11.4 -->
-    <script src="{{asset('dist/jquery-ui-1.13.1/jquery-ui.min.js')}}"></script>
+    <script src="{{ asset('dist/jquery-ui-1.13.1/jquery-ui.min.js') }}"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
         $.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 5 -->
-    <script src="{{asset('dist/bootstrap-5.0.2-dist/js/bootstrap.bundle.js')}}"></script>
+    <script src="{{ asset('dist/bootstrap-5.0.2-dist/js/bootstrap.bundle.js') }}"></script>
 
     @yield('POS_END')
 </body>
