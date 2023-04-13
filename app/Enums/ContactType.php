7e497975fb64c49f025579402ba6c13840a73ded
Akrez <akrez.like@gmail.com>
2023-04-14 02:54:12 +0330

    fix

--- app/Enums/ContactType.php
+++ app/Enums/ContactType.php
@@ -8,7 +8,7 @@ class ContactType extends Enum
 {
     public const CONTACT_TYPE_ADDRESS = 'address';
     public const CONTACT_TYPE_TELEGRAM = 'telegram';
-    public const CONTACT_TYPE_WHATSUP = 'whatsup';
+    public const CONTACT_TYPE_WHATSAPP = 'whatsapp';
     public const CONTACT_TYPE_PHONE = 'phone';
     public const CONTACT_TYPE_EMAIL = 'email';
     public const CONTACT_TYPE_INSTAGRAM = 'instagram';
@@ -18,7 +18,7 @@ class ContactType extends Enum
         return [
             static::CONTACT_TYPE_ADDRESS => __('Address'),
             static::CONTACT_TYPE_TELEGRAM => __('Telegram'),
-            static::CONTACT_TYPE_WHATSUP => __('Whatsup'),
+            static::CONTACT_TYPE_WHATSAPP => __('Whatsapp'),
             static::CONTACT_TYPE_PHONE => __('Phone'),
             static::CONTACT_TYPE_EMAIL => __('Email'),
             static::CONTACT_TYPE_INSTAGRAM => __('Instagram'),
